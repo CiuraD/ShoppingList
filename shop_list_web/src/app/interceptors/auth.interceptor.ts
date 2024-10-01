5dc173d5c3207f6e14bddc09b71efd703f91045f
CiuraD <127660370+CiuraD@users.noreply.github.com>
2024-10-01 23:37:21 +0200

    added jwt expiration checker

--- shop_list_web/src/app/interceptors/auth.interceptor.ts
+++ shop_list_web/src/app/interceptors/auth.interceptor.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {LocalStorageService} from "../services/local-storage/loacal-storage.service";
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {Router} from '@angular/router';
 
 @Injectable({
     providedIn: 'root'
@@ -10,12 +12,19 @@ export class AuthInterceptor implements HttpInterceptor {
 
     constructor(
         private readonly localStorageService: LocalStorageService,
+        private readonly jwtHelper: JwtHelperService,
+        private readonly router: Router,
     ) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const token = this.localStorageService.getString(LocalStorageService.TOKEN_KEY);
 
         if (token) {
+            if (this.jwtHelper.isTokenExpired(this.localStorageService.getString(LocalStorageService.TOKEN_KEY))) {
+                this.localStorageService.unset(LocalStorageService.TOKEN_KEY);
+                this.router.parseUrl('/login');
+                return next.handle(req);
+            }
             req = req.clone({
                 setHeaders: { Authorization: `Bearer ${token}` }
             });
