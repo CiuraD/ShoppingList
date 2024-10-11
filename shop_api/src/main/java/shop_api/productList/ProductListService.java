e6054e4f2ec4de4aece11ab71c0f3e762c9aa091
CiuraD <127660370+CiuraD@users.noreply.github.com>
2024-10-11 21:09:08 +0200

    Backend logic for sharing lists

--- shop_api/src/main/java/shop_api/productList/ProductListService.java
+++ shop_api/src/main/java/shop_api/productList/ProductListService.java
@@ -3,9 +3,12 @@ package shop_api.productList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import shop_api.product.ProductRepository;
+import shop_api.userGroup.UserGroup;
+import shop_api.userGroup.UserGroupRepository;
 
 @Service
 public class ProductListService {
@@ -15,6 +18,9 @@ public class ProductListService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private UserGroupRepository userGroupRepository;
 
     public List<ProductList> getAllProductLists() {
         return roductListRepository.findAll();
@@ -31,5 +37,22 @@ public class ProductListService {
     public void deleteProductList(String id) {
         productRepository.deleteAllByProductListId(id);
         roductListRepository.deleteById(id);
+    }
+
+    public ResponseEntity<String> shareListWithGroup(String productListId, String groupId) {
+        ProductList productList = roductListRepository.findById(productListId).orElse(null);
+        if (productList != null) {
+            UserGroup userGroup = userGroupRepository.findById(groupId).orElse(null);
+            if (userGroup != null) {
+                userGroup.addProductList(productListId);
+                userGroupRepository.save(userGroup);
+                
+                productList.setUserGroupId(groupId);
+                roductListRepository.save(productList);
+                return ResponseEntity.ok().body("Product list shared with group");
+            }
+            return ResponseEntity.status(404).body("Group not found");
+        }
+        return ResponseEntity.status(404).body("Product list not found");
     }
 }
