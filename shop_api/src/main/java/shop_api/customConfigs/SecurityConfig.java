a6f003e21d601ef5e05b0a69fa8cea9fd3be4702
CiuraD <127660370+CiuraD@users.noreply.github.com>
2024-12-19 20:12:12 +0100

    added swagger

--- shop_api/src/main/java/shop_api/customConfigs/SecurityConfig.java
+++ shop_api/src/main/java/shop_api/customConfigs/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig implements WebMvcConfigurer {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/api/users/login", "/api/users/register", "/error").permitAll()
+                        .requestMatchers("/api/users/login", "/api/users/register", "/error", "/swagger-ui/**", "/v3/**").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
